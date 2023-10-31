edd6eed864e04d79522473e8db15c41adce2d845
joyhasan08 <rubayedhasan235@gmail.com>
2023-10-31 14:57:08 +0600

    form fuction added

--- CarDoctor/Car-Doctor-clint/src/Page/Appointmment/Appoinment.jsx
+++ CarDoctor/Car-Doctor-clint/src/Page/Appointmment/Appoinment.jsx
@@ -1,18 +1,30 @@
 
 
 const Appoinment = () => {
+
+    const handelApponintment = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const model = e.target.model.value;
+        const date = e.target.date.value;
+        const description = e.target.description.value;
+        const info = { name, model, date, description }
+        console.log(info);
+
+
+    }
     return (
         <>
             <p className="text-4xl font-semibold text-center">Appointment page </p>
-            <form className="flex flex-col w-1/2 mx-auto space-y-2" >
+            <form onSubmit={handelApponintment} className="flex flex-col w-1/2 mx-auto space-y-2" >
                 <label htmlFor="">Your Name</label>
-                <input className="input input-sm input-bordered" type="text" name="" id="" />
+                <input name="name" className="input input-sm input-bordered" type="text" id="" />
                 <label htmlFor="">Car model</label>
-                <input className="input input-sm input-bordered" type="text" />
+                <input name="model" className="input input-sm input-bordered" type="text" />
                 <label htmlFor="">On date</label>
-                <input className="input input-sm input-bordered" type="number" />
+                <input name="date" className="input input-sm input-bordered" type="number" />
                 <label htmlFor="">Description</label>
-                <textarea className="input input-sm input-bordered" name="" id="" cols="20" rows="30"></textarea>
+                <textarea name="description" className="input input-sm input-bordered" id="" cols="20" rows="30"></textarea>
                 <input className="btn btn-success" type="submit" />
             </form>
         </>
--- CarDoctor/Car-Doctor-clint/src/Page/Home/AboutService.jsx
+++ CarDoctor/Car-Doctor-clint/src/Page/Home/AboutService.jsx
@@ -4,14 +4,15 @@ import part from "./../../assets/images/about_us/parts.jpg"
 const AboutService = () => {
     return (
         <div className="flex">
-            <div className="w-1/2">
+            <div className="w-1/2 m-4 relative">
                 <img src={parson} alt="" />
+                <img className=" absolute -bottom-4 -right-4 border-4 border-white w-1/2" src={part} alt="" />
             </div>
-            <div className="w-1/2">
-                <p>About us</p>
-                <h2>We are qualified & of experience in this field</h2>
+            <div className="w-1/2 p-4 space-y-4 card-body shadow-md">
+                <p className="font-medium text-xl">About us</p>
+                <h2 className="font-semibold text-2xl" >We are qualified & of experience in this field</h2>
                 <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                <button>Get More</button>
+                <button className="btn btn-outline">Get More</button>
             </div>
         </div>
     );
