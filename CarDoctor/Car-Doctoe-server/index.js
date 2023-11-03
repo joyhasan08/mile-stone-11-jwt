b9f7d2dd6125271f9618d719881118ded8f03674
joyhasan08 <rubayedhasan235@gmail.com>
2023-11-04 00:36:03 +0600

    database collaction added

--- CarDoctor/Car-Doctoe-server/index.js
+++ CarDoctor/Car-Doctoe-server/index.js
@@ -29,8 +29,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        //database_Creation
+        const CarData = client.db("carShop").collection("services")
 
         //Api_here
+        app.get("/services", async (req, res) => {
+            const cursor = CarData.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
